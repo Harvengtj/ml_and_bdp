d5180504c6239b1393bc4e507d983c30ce14d609
Justin <justin.harvengt@student.uclouvain>
2026-04-30 15:19:16 +0200

    dataset.py added and update main.py

---
+++ official/regression/src/dataset.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import scipy.io as sio
+
+#=======================================================================================
+#=== Dataset ===
+#=======================================================================================
+class BSDSDataset(Dataset):
+    def __init__(self, img_dir, gt_dir, transform=None, target_transform=None):
+        self.img_dir = img_dir # image directory
+        self.gt_dir = gt_dir # ground truth directory
+        self.transform = transform
+        self.target_transform = target_transform
+        
+        self.images = sorted(os.listdir(img_dir))
+        self.gts = sorted(os.listdir(gt_dir))
+        
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        # === Image (.jpg) ===
+        img_path = os.path.join(self.img_dir, self.images[idx]) # ex: dataset\images\test\2018
+        image = Image.open(img_path).convert("RGB")
+        
+        # === Ground truth (.mat) ===
+        gt_path = os.path.join(self.gt_dir, self.gts[idx]) # ex: dataset\ground_truth\test\2018
+        mat = sio.loadmat(gt_path)
+        gt = mat['groundTruth']
+        # In the event that gt is a segmentation list --> we take the first one
+        if isinstance(gt, np.ndarray) and gt.dtype == 'O':
+            gt = gt[0][0][0][0].astype(np.int64)
+        else:
+            gt = np.squeeze(gt)
+            
+        # === Transforms ===
+        if self.transform:
+            image = self.transform(image)
+            
+        gt = torch.tensor(gt, dtype=torch.long)
+        
+        return image, gt
+    
+    
+            
+        
+         
+        
+        
+            
--- official/regression/src/main.py
+++ official/regression/src/main.py
@@ -1,13 +1,19 @@
+import os
 import numpy as np
+import scipy.io as sio
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
+import torchvision.transforms as T
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from PIL import Image
+
+from dataset import BSDSDataset
 
 
 # Parameters
@@ -16,20 +22,32 @@ num_epochs = 5
 device = 'cuda:0'
 num_classes = 10
 
-# Load dataset
-transform = torchvision.transforms.Compose(
-    [torchvision.transforms.ToTensor(),
-     torchvision.transforms.Normalize((0.5), (0.5))])
+transform = T.Compose([
+    T.Resize((256, 256)),
+    T.ToTensor()
+])
 
-trainset = torchvision.datasets.MNIST(root='./data', train=True,
-                                        download=True, transform=transform)
-valset = torchvision.datasets.MNIST(root='./data', train=False,
-                                       download=True, transform=transform)
 
-print(trainset)
+trainset = BSDSDataset(
+    r"C:\Users\justi\OneDrive\Documents\Q10\machine_learning_and_big_data_processing\project\official\regression\dataset\images\train",
+    r"C:\Users\justi\OneDrive\Documents\Q10\machine_learning_and_big_data_processing\project\official\regression\dataset\ground_truth\train",
+    transform=transform
+)
 
-# Create dataloaders
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True)
-valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
-                                         shuffle=False)
+valset = BSDSDataset(
+    r"C:\Users\justi\OneDrive\Documents\Q10\machine_learning_and_big_data_processing\project\official\regression\dataset\images\val",
+    r"C:\Users\justi\OneDrive\Documents\Q10\machine_learning_and_big_data_processing\project\official\regression\dataset\ground_truth\val",
+    transform=transform
+)
+
+train_loader = DataLoader(trainset,
+                          batch_size=batch_size,
+                          shuffle=True)
+
+val_loader = DataLoader(valset,
+                          batch_size=batch_size,
+                          shuffle=False)
+
+print(train_loader)
+
+
