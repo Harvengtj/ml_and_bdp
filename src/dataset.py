3b0f59894c84bf34d75f72ee514a700425b8409c
Juju171 <80103242+Juju171@users.noreply.github.com>
2026-05-02 13:03:32 +0200

    LAB image viewer

--- src/dataset.py
+++ src/dataset.py
@@ -3,85 +3,69 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
-import scipy.io as sio
+from skimage.color import rgb2lab, lab2rgb
 
-#=======================================================================================
-#=== Dataset ===
-#=======================================================================================
 class ColoredDataset(Dataset):
-    def __init__(self, img_dir, transform=None, target_transform=None):
-        self.img_dir = img_dir # image directory
-        #self.gt_dir = gt_dir # ground truth directory
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
         self.transform = transform
-        self.target_transform = target_transform
-        
         self.images = sorted(os.listdir(img_dir))
-        #self.gts = sorted(os.listdir(gt_dir))
         
     def __len__(self):
         return len(self.images)
     
     def __getitem__(self, idx):
-        # === Image (.jpg) ===
-        img_path = os.path.join(self.img_dir, self.images[idx]) # ex: dataset\images\test\2018
+        img_path = os.path.join(self.img_dir, self.images[idx])
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+class GrayscaleBSDSDataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+        self.images = sorted(os.listdir(img_dir))
+        
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.images[idx])
+        image = Image.open(img_path).convert("L")
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+class LabBSDSDataset(Dataset):
+    """
+    Dataset that returns images in LAB color space.
+    L channel: Input (Grayscale)
+    AB channels: Target (Color information)
+    """
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+        self.images = sorted(os.listdir(img_dir))
+        
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.images[idx])
         image = Image.open(img_path).convert("RGB")
         
-        # === Ground truth (.mat) ===
-        #gt_path = os.path.join(self.gt_dir, self.gts[idx]) # ex: dataset\ground_truth\test\2018
-        #mat = sio.loadmat(gt_path)
-        #gt = mat['groundTruth']
-        # Extraction
-        #gt = gt[0][0]['Segmentation'][0][0]
-        #gt = gt.astype(np.int64)
-            
-        # === Transforms ===
+        # Apply transforms (Resize, etc.) before converting to LAB
         if self.transform:
             image = self.transform(image)
             
-        #gt = torch.tensor(gt, dtype=torch.long)
+        # Convert to numpy for skimage (expects 0-1 or 0-255)
+        img_np = np.array(image).transpose(1, 2, 0) # CHW to HWC
+        img_lab = rgb2lab(img_np)
         
-        return image
-    
-    
-    
-class GrayscaleBSDSDataset(Dataset):
-    def __init__(self, img_dir, transform=None, target_transform=None):
-        self.img_dir = img_dir # image directory
-        #self.gt_dir = gt_dir # ground truth directory
-        self.transform = transform
-        self.target_transform = target_transform
+        # L channel is in range [0, 100], AB channels in [-128, 127]
+        # Standardize to roughly [-1, 1] or [0, 1] for the model
+        img_lab[:, :, 0] = img_lab[:, :, 0] / 100.0          # L: [0, 1]
+        img_lab[:, :, 1:] = (img_lab[:, :, 1:] + 128) / 255.0 # AB: [0, 1]
         
-        self.images = sorted(os.listdir(img_dir))
-        #self.gts = sorted(os.listdir(gt_dir))
-        
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, idx):
-        # === Image (.jpg) ===
-        img_path = os.path.join(self.img_dir, self.images[idx]) # ex: dataset\images\test\2018
-        image = Image.open(img_path).convert("L")
-        
-        # === Ground truth (.mat) ===
-        #gt_path = os.path.join(self.gt_dir, self.gts[idx]) # ex: dataset\ground_truth\test\2018
-        #mat = sio.loadmat(gt_path)
-        #gt = mat['groundTruth']
-        # Extraction
-        #gt = gt[0][0]['Segmentation'][0][0]
-        #gt = gt.astype(np.int64)
-            
-        # === Transforms ===
-        if self.transform:
-            image = self.transform(image)
-            
-        #gt = torch.tensor(gt, dtype=torch.long)
-        
-        return image
-    
-    
-            
-        
-         
-        
-        
-            
+        return torch.from_numpy(img_lab.transpose(2, 0, 1)).float()
